aee5449c1fba6d4949033698e90e33523ba13e2c
Jitendra Verma <jitendra.verma@intimetec.com>
2025-08-13 17:16:18 +0530

    Operators

---
+++ Operators.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main()
+{
+    // Operators
+    printf("We have following types of operators:-\n");
+    printf("1.Arithmetic operators \n2.Assignment operators \n3.Comparison operators \n4.Logical operators \n5.Bitwise operators\n");
+    printf("--------------------------------------------");
+    printf("\n");
+    printf("Arithmetic Operators:\n");
+    printf("3+1 = %d \n",3+1);
+    printf("3-1 = %d \n",3-1);
+    printf("3*1 = %d \n",3*1);
+    printf("3/1 = %d \n",3/1);
+    printf("3MOD1 = %d \n",3%1);
+    int a=3;
+    printf("3++ = %d next time it will be 4\n",a++);
+    printf("3-- = %d \n",a--);
+}
